7c79304d83599511c8f1222b0478b2b08750b12e
uc-chilupyr <chilupyr@mail.uc.edu>
2021-01-23 12:56:24 -0500

    My First Commit

---
+++ bash/myfirstscript.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# This script outputs the IP address and Hostname of the machine
+echo 'This is a script. Hello!'
+greeting="This is a script. Hello!"
+echo $greeting,thanks for joining us!
+echo 'greeting, thanks for joining us! you owe me $20'
+echo "$greeting, thanks for joining us!"
+echo "$greeting, you owe me \$20"
+
+echo Machine Type: $MACHTYPE
+echo Hostname: $HOSTNAME
+echo Working Dir: $PWD
+echo Session length: $SECONDS
+echo Home Dir: $HOME
+
+a=$(ip a | grep 'dynamic ens192' | awk '{print $2}')
+echo My IP is $a
+
